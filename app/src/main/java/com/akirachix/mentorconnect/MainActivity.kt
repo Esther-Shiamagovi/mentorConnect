127fcf144bd310c7841ae6433e5b7b74bbba55b1
Esther-Shiamagovi <shiamagoviesther@gmail.com>
2024-06-05 20:07:25 +0300

    made changes

--- app/src/main/java/com/akirachix/mentorconnect/MainActivity.kt
+++ app/src/main/java/com/akirachix/mentorconnect/MainActivity.kt
@@ -6,18 +6,87 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-
+import com.akirachix.mentorconnect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val connectRegister=findViewById<TextView>(R.id.textView2)
-        connectRegister.setOnClickListener {
-            val intent = Intent( this,second_activity::class.java)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView2.setOnClickListener {
+            val intent =Intent(this,second_activity::class.java)
             startActivity(intent)
+        }
+        binding.button.setOnClickListener {
+            validateRegistration()
         }
 
     }
+    fun validateRegistration(){
+        clearErrors()
+        var formError = false
+        val firstName= binding.etFirstName.text.toString()
+        if (firstName.isBlank()){
+            formError = true
+        }
+        binding.tilFirstName.error = getString(R.string.first_name_is_required)
+        val lastName= binding.etLastName.text.toString()
+        if(lastName.isBlank()){
+            formError= true
+            binding.tilLastName.error = getString(R.string.last_name_is_required)
+        }
+        val email = binding.etEmail.text.toString()
+        if (email.isBlank()) {
+            formError = true
+            binding.tilEmail.error = getString(R.string.email_is_required)
+        }
+        val codehiveId = binding.etCodehiveId.text.toString()
+        if (email.isBlank()) {
+            formError = true
+            binding.tilCodehiveId.error = getString(R.string.codehiveid_is_required)
+        }
+            val userName = binding.etUserName.text.toString()
+        if (userName.isBlank()) {
+            formError = true
+            binding.tilUserName.error = getString(R.string.username_is_required)
+        }
+        val password = binding.etPassword.text.toString()
+        if (password.isBlank()) {
+            formError = true
+            binding.tilUserName.error = getString(R.string.password_is_required)
+        }
+        val confirmpassword = binding.etCon.text.toString()
+        if (confirmpassword.isBlank()) {
+            formError = true
+            binding.tilConfirmPassword.error = "password is required"
+        }
+        if (password!=confirmpassword) {
+            formError = true
+            binding.tilConfirmPassword.error =
+                getString(R.string.password_and_confirmation_do_not_match)
+        }
+        if(formError){
+            formError = true
+        }
 
+    }
+    fun clearErrors(){
+        binding.tilFirstName.error = null
+        binding.tilLastName.error = null
+        binding.tilEmail.error = null
+        binding.tilCodehiveId.error = null
+        binding.tilUserName.error = null
+        binding.tilPassword.error = null
+        binding.tilConfirmPassword.error = null
+    }
 }
+
+//        setContentView(R.layout.activity_main)
+//        val connectRegister=findViewById<TextView>(R.id.textView2)
+//        connectRegister.setOnClickListener {
+//            val intent = Intent( this,second_activity::class.java)
+//            startActivity(intent)
+//        }
--- app/src/main/java/com/akirachix/mentorconnect/second_activity.kt
+++ app/src/main/java/com/akirachix/mentorconnect/second_activity.kt
@@ -7,14 +7,45 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.akirachix.mentorconnect.databinding.ActivityMainBinding
+import com.akirachix.mentorconnect.databinding.ActivitySecondBinding
 
 class second_activity : AppCompatActivity() {
+    lateinit var binding:ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
-        val connectRegister = findViewById<TextView>(R.id.textView4)
-        connectRegister.setOnClickListener {finish()
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.textView4.setOnClickListener {
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+            finish()}
+            binding.button2.setOnClickListener {
+                validateForm()
+            }
+    }
+//    replace view id with viewbinding
+    fun validateForm(){
+        removeError()
+        var formError = false
+        val firstName= binding.etSecUsername.text.toString()
+        if (firstName.isBlank()){
+            formError = true
         }
+        binding.tilSecUsername.error = "User name is required"
+        val password= binding.etSecPassword.text.toString()
+        if (password.isBlank()){
+            formError = true
+        }
+        binding.tilSecPassword.error = "password is required"
+    }
+    fun removeError(){
+        binding.tilSecUsername.error = null
+        binding.tilSecPassword.error = null
     }
 }
+//        setContentView(R.layout.activity_second)
+//        val connectRegister = findViewById<TextView>(R.id.textView4)
+//        connectRegister.setOnClickListener {finish()
+//        }
